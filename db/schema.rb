9b151e53b56322ee3597a803204cc150ab7172dd
Serhii T <tocarchuk@gmail.com>
2021-12-14 21:42:06 +0200

    Latest update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_141930) do
+ActiveRecord::Schema.define(version: 2021_12_14_144350) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -20,4 +20,17 @@ ActiveRecord::Schema.define(version: 2021_12_14_141930) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.datetime "departure"
+    t.integer "duration"
+    t.integer "origin_id", null: false
+    t.integer "destination_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["destination_id"], name: "index_flights_on_destination_id"
+    t.index ["origin_id"], name: "index_flights_on_origin_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "destination_id"
+  add_foreign_key "flights", "airports", column: "origin_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,29 @@
 airports = []
   airports[0] = Airport.create(code: 'ODS', name: 'Odesa International Airport', city: 'Odesa')
   airports[1] = Airport.create(code: 'IEV', name: 'Kiev Zhulhany Airport', city: 'Kyiv')
-  airports[2] = Airport.create(code: 'KBP', name: 'Kiev Borispol Airport', city: 'Kyiv')
+
+DURATIONS ={
+  'IEV' => {
+    'ODS' => 80
+  },
+  'ODS' => {
+    'IEV' => 80
+  }
+}
+
+def get_duration(origin, destination)
+  DURATIONS[origin][destination] || DURATIONS[destination][origin]
+end
+
+airports.each do |origin|
+  airports.each do |destination|
+    next if origin == destination
+
+    2.times { Flight.create(departure: Date.new(2021, 12, 14),
+                            origin_id: origin.id,
+                            destination_id: destination.id,
+                            duration: get_duration(origin.code, destination.code)
+                            )
+    }
+  end
+end
