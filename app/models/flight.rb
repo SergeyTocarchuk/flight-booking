62dee762e247e652f06cd827a441cc0512ea454a
Serhii T <tocarchuk@gmail.com>
2021-12-16 17:32:58 +0200

    Update schema

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,7 @@ class Flight < ApplicationRecord
   attr_accessor :tickets
   belongs_to :origin, class_name: "Airport"
   belongs_to :destination, class_name: "Airport"
+  has_many :bookings
 
   def date_formatted
     departure.strftime("%m.%d.%Y")
